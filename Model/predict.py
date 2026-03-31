b4370317e2cf6a30d540a0b1c83021fa060dcf8f
Adnan Saeed <adnansaeed.datascientist@gmail.com>
2026-03-31 23:47:17 +0500

    little bit changes in predict.py

--- Model/predict.py
+++ Model/predict.py
@@ -7,9 +7,6 @@ with open('Model/model.pkl', 'rb') as f:
 MODEL_VERSION = '1.0.0'
 
 def predict_it(data: dict):
-
     data = pd.DataFrame([data])
-
     prediction = model.predict(data)[0]
-    
     return prediction
